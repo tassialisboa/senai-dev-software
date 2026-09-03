41966c7f19f1fa6d374b67664b4bf89a3ce54c0e
matheusvsantos8-a11y <matheus.v.santos8@ba.estudante.senai.br>
2026-09-02 21:23:00 -0300

    Aula dia 02/09

--- MinhaApi/Repositories/ProdutoRepository.cs
+++ MinhaApi/Repositories/ProdutoRepository.cs
@@ -6,6 +6,8 @@ using MySqlConnector;
 
 public class ProdutoRepository : IProdutoRepository
 {
+    private readonly string _connectionString; 
+    public ProdutoRepository(IConfiguration config) => _connectionString = config.GetConnectionString("DefaultConnection")!;
     private static List<Produto> _db = new()
     {
         new Produto { Id=1, Nome="Notebook", Preco=2500m, Estoque=10 },
@@ -15,42 +17,64 @@ public class ProdutoRepository : IProdutoRepository
 
     public IEnumerable<Produto> GetAll()
     {
-        public class ProdutoRepository : IProdutoRepository { 
-            private readonly string _connectionString; 
-            public ProdutoRepository(IConfiguration config) => _connectionString = config.GetConnectionString("DefaultConnection")!;
-            public IEnumerable<Produto> GetALL() { var lista = new List<Produto>();
-            using var conn = new MySqlConnection(_connectionString); conn.Open();
-            string sql = "Select id, nome, preco, estoque, ativo From produtos";
-            using var cmd = new MySqlCommand(sql, conn);
-            using var reader = cmd.ExecuteReader();
-            while(reader,Read()) {
-                lista.Add(new Produto
-                { Id = reader.GetInt32("id"),
-                Nome = reader.GetString("nome"),
-                Preco = reader.GetDecimal("preco"),
-                Estoque = reader.GetInt32("estoque"),
-                 Ativo = reader.GetBoolean("ativo")
-                })
-            }
-            }
-            return lista;
+       
+        var lista = new List<Produto>();
+        using var conn = new MySqlConnection(_connectionString); conn.Open();
+        string sql = "Select id, nome, preco, estoque, ativo From produtos";
+        using var cmd = new MySqlCommand(sql, conn);
+        using var reader = cmd.ExecuteReader();
+        while(reader.Read()) {
+            lista.Add(new Produto
+            { Id = reader.GetInt32("id"),
+            Nome = reader.GetString("nome"),
+            Preco = reader.GetDecimal("preco"),
+            Estoque = reader.GetInt32("estoque"),
+                Ativo = reader.GetBoolean("ativo")
+            });
         }
+        
+        return lista;
+        
     } 
 
     public Produto? GetById(int id) => _db.FirstOrDefault(p => p.Id == id);
 
     public void Add(Produto p)
+    
     {
-        p.Id = _db.Any() ? _db.Max(x => x.Id) + 1 : 1;
-        _db.Add(p);
+        using var conn = new MySqlConnection(_connectionString); conn.Open();
+        string sql = "Insert Into produtos (nome, preco, estoque, ativo) Values (@nome, @preco, @estoque, @ativo) VALUES (@Nome, @Preco, @Estoque, @Ativo); SELECT LAST_INSERT_ID();";
+        using var cmd = new MySqlCommand(sql, conn);
+        cmd.Parameters.AddWithValue("@nome", p.Nome);
+        cmd.Parameters.AddWithValue("@preco", p.Preco);
+        cmd.Parameters.AddWithValue("@estoque", p.Estoque);
+        cmd.Parameters.AddWithValue("@ativo", p.Ativo);
+       var idGerado = cmd.ExecuteNonQuery();
+       p.Id = Convert.ToInt32(idGerado);
+       
     }
 
     public void Update(Produto p)
     {
-        var i = _db.FindIndex(x => x.Id == p.Id);
-        if (i >= 0) _db[i] = p;
+        using var conn = new MySqlConnection(_connectionString); conn.Open();
+        String sql = "Update produtos Set nome=@nome, preco=@preco, estoque=@estoque, ativo=@ativo Where id=@id";
+        using var cmd = new MySqlCommand(sql, conn);
+        cmd.Parameters.AddWithValue("@id", p.Id);
+        cmd.Parameters.AddWithValue("@nome", p.Nome);
+        cmd.Parameters.AddWithValue("@preco", p.Preco);
+        cmd.Parameters.AddWithValue("@estoque", p.Estoque);
+        cmd.Parameters.AddWithValue("@ativo", p.Ativo);
+        cmd.ExecuteNonQuery();
     }
 
-    public void Delete(int id) => _db.RemoveAll(p => p.Id == id);
+    public void Delete(int id) 
+    {
+        using var conn = new MySqlConnection(_connectionString); conn.Open();
+        String sql = "Delete From produtos Where id=@id";
+        using var cmd = new MySqlCommand(sql, conn);
+        cmd.Parameters.AddWithValue("@id", id);
+        cmd.ExecuteNonQuery();
+    }
+    
 }
 
